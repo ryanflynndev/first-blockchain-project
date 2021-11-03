6fe8f465a16410104e53898bbaa10117414ab950
Ryan Flynn <ryanflynn44@gmail.com>
2021-11-02 22:01:49 -0400

    Transaction constructor finished

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
 import * as crpto from 'crypto';
+
+class Transaction {
+    constructor(
+        public amount: number,
+        public payer: string, // public key
+        public payee: string, // public key
+    ) {}
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+class Block {
+
+}
+
+class Chain {
+
+}
+
+class Wallet {
+
+}
