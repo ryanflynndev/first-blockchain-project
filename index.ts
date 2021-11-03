923da21d1fb155744426e305ec509c0b5317f86d
Ryan Flynn <ryanflynn44@gmail.com>
2021-11-02 22:06:15 -0400

    Finished block implementation

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as crpto from 'crypto';
+import * as crypto from 'crypto';
 
 class Transaction {
     constructor(
@@ -13,7 +13,18 @@ class Transaction {
 }
 
 class Block {
+    constructor(
+        public prevHash: string,
+        public transaction: Transaction,
+        public timestamp = Date.now(),
+    ){}
 
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex');
+    }
 }
 
 class Chain {
